fc8d61ffe94e4a6b95ecc8428916edde621a46d9
elombardi2 <eric.lombardi@liris.cnrs.fr>
2019-01-25 08:12:39 +0100

    Implement Enriched Polyhedron - WIP - Do NOT compile

--- FEVV/Filters/CGAL/Polyhedron/Boolean_Operations/boolean_enriched_polyhedron.hpp
+++ FEVV/Filters/CGAL/Polyhedron/Boolean_Operations/boolean_enriched_polyhedron.hpp
@@ -1,6 +1,8 @@
 //TODO-elo-licence
 #pragma once
 
+#include <CGAL/Cartesian.h>
+#include <CGAL/Polyhedron_3.h>
 #include <CGAL/Polyhedron_items_3.h>
 
 template <class Refs, class T, class P, class Norm>
@@ -55,8 +57,91 @@ class MEPP_Common_Vertex : public CGAL::HalfedgeDS_vertex_base<Refs, T, P>
 };
 
 
+template <class Refs, class Tprev, class Tvertex, class Tface, class Norm>
+class MEPP_Common_Halfedge : public CGAL::HalfedgeDS_halfedge_base<Refs,Tprev,Tvertex,Tface>
+{
+	protected:
+		// tag
+		int m_tag;
+
+#if (0)
+		// normal
+		Norm m_normal;  // AJOUT C�line : halfedge normal (sum of 2 incident facet normals)
+#endif
+
+		// option for edge superimposing
+		bool m_control_edge;
+
+		// texture coordinates : AJOUT Laurent Chevalier
+		float m_texture_coordinates[2];
+
+	public:
+		// life cycle
+		MEPP_Common_Halfedge()
+		{
+			m_control_edge = true;
+
+			// texture coordinates : AJOUT Laurent Chevalier
+			texture_coordinates(0.0f, 0.0f);
+		}
+
+		// tag
+		const int& tag() const { return m_tag;  }
+		int& tag() { return m_tag;  }
+		void tag(const int& t)  { m_tag = t; }
+
+#if (0)
+		// normal : AJOUT C�line
+		typedef Norm Normal_3;
+		Normal_3& normal() { return m_normal; }
+		const Normal_3& normal() const { return m_normal; }
+#endif
+
+		// texture coordinates : AJOUT Laurent Chevalier
+		float texture_coordinates(int index) { return m_texture_coordinates[index]; }
+		void texture_coordinates(float u, float v) { m_texture_coordinates[0] = u; m_texture_coordinates[1] = v; }
+
+		// control edge
+		bool& control_edge()  { return m_control_edge; }
+		const bool& control_edge()  const { return m_control_edge; }
+		void control_edge(const bool& flag) { m_control_edge  = flag; }
+};
 
 
+template <class Refs, class T, class Norm>
+class MEPP_Common_Facet : public CGAL::HalfedgeDS_face_base<Refs, T>
+{
+	protected:
+		// tag
+		int m_tag;
+
+		// normal
+		Norm m_normal;
+
+		// color
+		float m_color[3];
+
+	public:
+		// life cycle
+		MEPP_Common_Facet()
+		{
+			color(0.5f, 0.5f, 0.5f);
+		}
+
+		// tag
+		const int& tag() const { return m_tag;  }
+		int& tag() { return m_tag;  }
+		void tag(const int& t)  { m_tag = t; }
+
+		// normal
+		typedef Norm Normal_3;
+		Normal_3& normal() { return m_normal; }
+		const Normal_3& normal() const { return m_normal; }
+
+		// color
+		float color(int index) { return m_color[index]; }
+		void color(float r, float g, float b) { m_color[0] = r; m_color[1] = g; m_color[2] = b; }
+};
 
 
 template <class Refs, class T, class P, class Norm>
